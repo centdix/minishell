0bc09ce18099b81128e6a55aa21b82310abdcf6e
Leo MARTIN <lmartin@e2r7p22.42.fr>
2019-12-08 04:00:41 +0100

    .

---
+++ includes/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/07 23:48:25 by lmartin           #+#    #+#             */
+/*   Updated: 2019/12/07 23:57:11 by lmartin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <dirent.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/errno.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
