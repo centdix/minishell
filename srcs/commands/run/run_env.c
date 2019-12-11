8987818cbc5b4dd7775e6f07862c01dd2f21e499
Leo MARTIN <lmartin@e2r12p1.42.fr>
2019-12-11 06:15:35 +0100

    norm env

--- srcs/commands/run/run_env.c
+++ srcs/commands/run/run_env.c
@@ -6,7 +6,7 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 02:25:01 by lmartin           #+#    #+#             */
-/*   Updated: 2019/12/11 02:34:35 by lmartin          ###   ########.fr       */
+/*   Updated: 2019/12/11 06:14:37 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@
 
 int		env_set_value(char **value, char *begin, char **data)
 {
-	while (*(*data) && !ft_isspace(*(*data)))
-		(*data)++;
-	if (**data)
-		*(*data)++ = '\0';
-	if ((*value = ft_strdup(begin)) < 0)
-		return (-1);
+	if (!*(*data) || ft_isspace(*(*data)))
+		*value = NULL;
+	else if ((begin = (*data)))
+	{
+		while (*(*data) && !ft_isspace(*(*data)))
+			(*data)++;
+		if (**data)
+			*(*data)++ = '\0';
+		if ((*value = ft_strdup(begin)) < 0)
+			return (-1);
+	}
 	return (0);
 }
 
@@ -31,29 +36,31 @@ int		env_set_value(char **value, char *begin, char **data)
 ** Parse data and add or modify existing env_variables
 */
 
-int		env_export_env(t_minishell *minishell, char **data)
+int		env_export_env(t_lstenv_v *env_variables, char **data)
 {
+	char		equal;
 	char		*begin;
 	char		*name;
 	char		*value;
 	t_lstenv_v	*envv;
 
-	begin = (*data);
-	if (*(*data) == '=')
-		return (-1);
-	while (*(*data) && *(*data) != '=' && !ft_isspace(*(*data)))
+	if ((!(begin = (*data)) || begin) && *(*data) == '=')
+		return (-2);
+	while ((equal = **data) && *(*data) != '=' && !ft_isspace(*(*data)))
 		(*data)++;
 	if (*(*data))
 		*(*data)++ = '\0';
 	if ((name = ft_strdup(begin)) < 0)
 		return (-1);
-	if (!*(*data) || ft_isspace(*(*data)))
-		value = NULL;
-	else if ((begin = (*data)) && env_set_value(&value, begin, &(*data)) < 0)
+	if (!(envv = get_env_variable(env_variables, name)) && equal != '=')
+		return (-2);
+	if (equal != '=')
+		return (1);
+	if (env_set_value(&value, begin, &(*data)) < 0)
 		return (-1);
-	if ((envv = get_env_variable(minishell->env_variables, name)))
+	if ((envv = get_env_variable(env_variables, name)))
 		envv->value = value;
-	else if (add_back_env(&minishell->env_variables, name, value) < 0)
+	else if (add_back_env(&env_variables, name, value) < 0)
 		return (-1);
 	return (0);
 }
@@ -76,12 +83,44 @@ int		env_write_envv(t_lstenv_v *envv)
 }
 
 /*
+** Add new envv with data in a envv buffer (not in the minishell's one)
+*/
+
+int		add_and_write_envv(t_minishell *minishell)
+{
+	int			ret;
+	t_lstenv_v	*envv;
+	t_lstenv_v	*new;
+	char		*data;
+
+	new = NULL;
+	envv = minishell->env_variables;
+	while (envv && (data = minishell->commands->data))
+		if (add_back_env(&new, envv->name, envv->value) < 0 ||
+(!(envv = envv->next) && envv))
+			return (-1);
+	while (*data)
+	{
+		if ((ret = env_export_env(new, &data)) != 0)
+			return (ret); // A FAIRE GESTION ERREURS
+		data = (ft_isspace(*data)) ? data + 1 : data;
+	}
+	while (new && (envv = new))
+	{
+		if (env_write_envv(new) < 0 || (!(new = new->next) && new))
+			return (-1);
+		free(envv);
+	}
+	return (0);
+}
+
+/*
 ** Run export command
 */
 
 int		run_env(t_minishell *minishell)
 {
-	char		*data;
+	int			ret;
 	t_lstenv_v	*envv;
 
 	envv = minishell->env_variables;
@@ -96,14 +135,11 @@ int		run_env(t_minishell *minishell)
 	}
 	else
 	{
-		data = minishell->commands->data;
-		while (*data)
-		{
-			if (env_export_env(minishell, &data) < 0)
-				return (-1);
-			if (ft_isspace(*data))
-				data++;
-		}
+		ret = add_and_write_envv(minishell);
+		if (ret == -1)
+			return (ret);
+		else if (ret == -2)
+			return (0); // ERREUR A FAIRE NO SUCH FILE OR DIRECTORY
 	}
 	return (0);
 }
