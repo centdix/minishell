3afee81822b6fa09543a1a03bc9d9412efff56db
Leo MARTIN <lmartin@e1r3p4.42.fr>
2019-12-17 16:54:46 +0100

    error_message on cd

--- includes/errors.h
+++ includes/errors.h
@@ -6,7 +6,7 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 06:25:36 by lmartin           #+#    #+#             */
-/*   Updated: 2019/12/17 11:11:36 by lmartin          ###   ########.fr       */
+/*   Updated: 2019/12/17 16:51:47 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,11 @@
 # define ERR_MALLOC -7
 # define ERR_WRITE -8
 
-# define MSG_CMD_NOT_FOUND "command not found"
-# define MSG_WRONG_ARGS "wrong argument"
-# define MSG_NOT_ENOUGH_ARGS "not enough arguments"
-# define MSG_TOO_MANY_ARGS "too many arguments"
+# define MSG_CMD_NOT_FOUND "Command not found"
+# define MSG_WRONG_ARGS "Wrong argument"
+# define MSG_NOT_ENOUGH_ARGS "Not enough arguments"
+# define MSG_TOO_MANY_ARGS "Too many arguments"
+# define MSG_NO_SUCH
 
 /*
 ** Errors about command
