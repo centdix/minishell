db0b05b8c5defa8b6895038479216cca99e89890
Leo MARTIN <lmartin@e1r3p4.42.fr>
2019-12-17 18:27:09 +0100

    Error for all redirections

--- srcs/parsing/parsing_redirect_input.c
+++ srcs/parsing/parsing_redirect_input.c
@@ -6,11 +6,38 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 03:06:32 by lmartin           #+#    #+#             */
-/*   Updated: 2019/12/17 17:16:09 by lmartin          ###   ########.fr       */
+/*   Updated: 2019/12/17 17:44:04 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int		check_redirect_input_error(char **begin, char **line,
+char **data, char **name)
+{
+	int		ret;
+	void	*stat;
+
+	if (!(stat = malloc(sizeof(struct stat))))
+		return (-1);
+	if ((ret = lstat(*data, stat)))
+	{
+		*line = *begin;
+		(*line)++;
+		ft_multifree(stat, *data, *name, NULL);
+		return (NO_SUCH_FILE);
+	}
+	free(stat);
+	if ((ret = open(*data, O_RDONLY)) < 0 && !(opendir(*data)))
+	{
+		*line = *begin;
+		(*line)++;
+		ft_multifree(*data, *name, NULL, NULL);
+		return (PERM_DENIED);
+	}
+	close(ret);
+	return (0);
+}
 
 /*
 ** Parsing < (redirect_input)
@@ -18,7 +45,7 @@
 
 int		parsing_redirect_input(char **line, t_lstcommands **commands)
 {
-	void	*stat;
+	int		ret;
 	char	*begin;
 	char	*name;
 	char	*data;
@@ -30,16 +57,8 @@ int		parsing_redirect_input(char **line, t_lstcommands **commands)
 		return (-1);
 	if (!(data = get_data_no_space(line)))
 		return (-1);
-	if (!(stat = malloc(sizeof(struct stat))))
-		return (-1);
-	if (lstat(data, stat))
-	{
-		*line = begin;
-		(*line)++;
-		ft_multifree(stat, data, name, NULL);
-		return (NO_SUCH_FILE);
-	}
-	free(stat);
+	if ((ret = check_redirect_input_error(&begin, line, &data, &name)) < 0)
+		return (ret);
 	if ((add_back(commands, TYPE_RD_INPUT, name, data) < 0))
 		return (-1);
 	return (1);
--- srcs/parsing/parsing_redirect_simple_output.c
+++ srcs/parsing/parsing_redirect_simple_output.c
@@ -6,14 +6,44 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 03:03:29 by lmartin           #+#    #+#             */
-/*   Updated: 2019/12/17 09:04:11 by lmartin          ###   ########.fr       */
+/*   Updated: 2019/12/17 18:25:16 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+** Check error for redirect_simple_output
+*/
+
+int		check_error_simple_output(char **data, char **name, char **line,
+char **begin)
+{
+	int		ret;
+	void	*stat;
+
+	if (!(stat = malloc(sizeof(struct stat))))
+		return (-1);
+	if (opendir(*data) || (!lstat(*data, stat) &&
+(ret = open(*data, O_RDONLY)) < 0))
+	{
+		*line = *begin;
+		*line = &(*line)[2];
+		if (opendir(*data))
+		{
+			ft_multifree(*data, *name, stat, NULL);
+			return (IS_A_DIRECTORY);
+		}
+		ft_multifree(*data, *name, stat, NULL);
+		return (PERM_DENIED);
+	}
+	free(stat);
+	return (0);
+}
+
 int		parsing_redirect_simple_output(char **line, t_lstcommands **commands)
 {
+	int		ret;
 	char	*begin;
 	char	*name;
 	char	*data;
@@ -26,6 +56,8 @@ int		parsing_redirect_simple_output(char **line, t_lstcommands **commands)
 		return (-1);
 	if (!(data = get_data_no_space(line)))
 		return (-1);
+	if ((ret = check_error_simple_output(&data, &name, line, & begin)) < 0)
+		return (ret);
 	if ((add_back(commands, TYPE_RD_S_OUT, name, data) < 0))
 		return (-1);
 	return (1);
